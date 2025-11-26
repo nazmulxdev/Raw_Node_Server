8ca6f334647cfaccf9193fcff03bc5836070e986
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-11-27 02:37:51 +0600

    explore post and get method using raw node http module

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,39 @@ const server: Server = http.createServer(
         }),
       );
     }
+
+    // get request
+    if (req.url === "/api" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Health status ok",
+          path: req.url,
+        }),
+      );
+    }
+
+    // post request
+
+    if (req.url === "/api/users" && req.method === "POST") {
+      let body = "";
+      // listen for data chunk
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        try {
+          const data = JSON.parse(body);
+          console.log("Received Data : ", data);
+
+          res.writeHead(200, { "content-type": "application/json" });
+          res.end(JSON.stringify({ message: "processing..", data }));
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    }
   },
 );
 
